17a6a30a90fb0dbb873ab163cdc58cd8e41cb1fe
JavierVMC29 <javiervegamolina29@gmail.com>
2025-06-21 12:10:23 -0500

    feat: add google tag manager

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -15,6 +15,7 @@ import Footer from "@/components/sections/footer";
 import formatDescription from "@/lib/metadata/formatDescription";
 import { getTranslations } from "next-intl/server";
 import { GlobalMessageKeys } from "@/i18n/keys";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -96,6 +97,12 @@ export default async function LocaleLayout({ children, params }: { children: Rea
         <meta name="twitter:image" content={OpenGraphImage.src} />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7N23CMV" height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <NextIntlClientProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
             <Header />
@@ -103,6 +110,21 @@ export default async function LocaleLayout({ children, params }: { children: Rea
             <Footer />
           </ThemeProvider>
         </NextIntlClientProvider>
+
+        {/* SCRIPT DE GOOGLE TAG MANAGER */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K7N23CMV');
+        `}
+        </Script>
       </body>
     </html>
   );
